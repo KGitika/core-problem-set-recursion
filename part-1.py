664528f89535752cf56580976cb4335f0778c17d
Gitika Kalal <gitikakalal21@gmail.com>
2025-05-05 16:40:13 -0700

    completed part1 and part2

--- part-1.py
+++ part-1.py
@@ -4,17 +4,36 @@
 # the appropriate comment.
 
 # factorial
-
+def factorial(n):
+    if n < 0:
+        raise ValueError("Input must be a positive integer.")
+    if n == 0:
+        return 1
+    return n * factorial(n-1)
 
 
 # reverse
-
+def reverse(text):
+    if text == "":
+        return ""
+        
+    return text[-1] + reverse(text[:-1])
 
 
 # bunny
-
+def bunny (count):
+    if count == 0:
+        return 0
+    return 2 + bunny (count-1)
 
 
 # is_nested_parens
-
+def is_nested_parens(parens):
+    if parens == "":
+        return True
+        
+    if len(parens)%2 != 0 or parens[0] != '(' or parens[-1] != ')':
+        return False
+        
+    return is_nested_parens(parens[1:-1])
 
--- part-2.py
+++ part-2.py
@@ -4,13 +4,51 @@
 # the appropriate comment.
 
 # search
-
+def search(array, query):
+    if not array:
+        return False
+        
+    if array[0] == query:
+        return True
+        
+    return search(array[1:], query)
 
 
 # is_palindrome
-
+def is_palindrome(text):
+    if len(text) <= 1:
+        return True
+    
+    if text[0] != text[-1]:
+        return False
+        
+    return is_palindrome(text[1:-1])
 
 
 # digit_match
 
+def digit_match(apples, oranges):
+    
+    def helper (apples, oranges):
+        if(apples == 0 or oranges == 0):
+            return 0
+        match_count = 1 if (apples%10 == oranges%10) else 0
+            
+        return match_count + helper(apples//10, oranges//10)
+        
+    
+    if(apples == 0 and oranges == 0):
+        return 1
+    
+    return helper(apples, oranges)
+
+
+# def digit_match(apples, oranges):
+#     if(apples == 0 and oranges == 0):
+#         return 1
+#     if(apples == 0 or oranges == 0):
+#         return 0
+#     match_count = 1 if (apples%10 == oranges%10) else 0
+        
+#     return match_count + digit_match(apples//10, oranges//10)
 
